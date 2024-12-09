b7b1c5cf481474606514d6d790aac4357de10dff
palakkhatri2098@gmail.com <palakkhatri2098@gmail.com>
2024-12-09 14:02:16 +0530

    update .env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const MONGO_URL =
-  process.env.MONGO_URL || 'mongodb://localhost:27017/faceRecognitionDB';
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/faceRecognitionDB';
 export const PORT = process.env.PORT || 8080;
 export const MATCH_THRESHOLD = 0.55;
 export const UPLOAD_PATH = process.env.UPLOAD_PATH || 'tmp/';
