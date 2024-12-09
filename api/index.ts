49bd7c940b7e7376f5890edbc277aae8d6440620
palakkhatri2098@gmail.com <palakkhatri2098@gmail.com>
2024-12-09 11:10:12 +0530

    to check if issue lies in logic

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,11 @@
-import app from '../src/index';
-import { VercelRequest, VercelResponse } from '@vercel/node';
+// import app from '../src/index';
+// import { VercelRequest, VercelResponse } from '@vercel/node';
 
-// Wrap Express app as a Serverless Function
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req, res);
+// // Wrap Express app as a Serverless Function
+// export default (req: VercelRequest, res: VercelResponse) => {
+//   app(req, res);
+// };
+
+export default (req, res) => {
+  res.status(200).json({ message: 'Serverless function works!' });
 };
